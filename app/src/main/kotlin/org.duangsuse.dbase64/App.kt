b03ad23f58e9f66c7f51322c9934cd41cd23f8d0
duangsuse <fedora-opensuse@outlook.com>
2019-03-09 01:25:29 +0800

    Add icon, base main layout

--- app/src/main/kotlin/org.duangsuse.dbase64/App.kt
+++ app/src/main/kotlin/org.duangsuse.dbase64/App.kt
@@ -1,20 +1,24 @@
 package org.duangsuse.dbase64
 
 import android.os.Bundle
-import android.support.design.*
+import android.support.design.widget.*
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.*
 
 import android.view.*
+import android.widget.*
+import android.widget.FrameLayout.*
 
 class App: AppCompatActivity() {
   override fun onCreate(oldInstanceStat: Bundle?) {
     super.onCreate(oldInstanceStat)
+
+    setupUi(this)
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    //menuInflater.inflate(R.menu.main_menu, menu)
-    menu.let(::setupMenu)
+    menuInflater.inflate(R.menu.main_menu, menu)
+
     return true
   }
 
@@ -25,8 +29,24 @@ class App: AppCompatActivity() {
   }
 
   companion object {
-    fun setupMenu(m: Menu) {
-      m.addItem("a")
+    // bad practice
+    fun setupUi(app: App) {
+      val main = FrameLayout(app)
+
+      val text = EditText(app)
+      text.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+
+      main.addView(text)
+
+      val fab = FloatingActionButton(app)
+      val fabLayoutFmt = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+      fabLayoutFmt.gravity = Gravity.BOTTOM or Gravity.END
+
+      fab.layoutParams = fabLayoutFmt
+
+      main.addView(fab)
+
+      app.setContentView(main)
     }
   }
 }
